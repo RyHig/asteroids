763f07aaa6bdf57e58f1221655997afa377137a1
Ryan Higgins <ryanjeffhiggins@gmail.com>
2024-10-05 22:17:37 -0500

    Drawing a triangle...

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     running = True
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,7 +18,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_q] or keys[pygame.K_ESCAPE]:
+            running = False
+
         screen.fill("black")
+        player.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
